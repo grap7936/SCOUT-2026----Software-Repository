a00b262cbb392b9fdaa96ab9d3eaca5cd2c7fb78
grap7936 <grap7936@colorado.edu>
2026-06-24 10:59:07 -0600

    Sentry.hpp

--- INSTR/include/Sentry.hpp
+++ INSTR/include/Sentry.hpp
@@ -22,6 +22,11 @@ public:
     Selector selector;
 
 private:
+    int DEBRIS_THRESHOLD;
+    int REFRESH_FREQUENCY;
+
+    int current_frame_number;
+    int frame_timeout;
     bool is_first_save = true; // used in writeTargetsToFile to determine if a new text file must be created to write information into 
                                // -- this starts as true so that the 1st save creates new info and then is changed in the riteTargetsToFile() 
                                // function to False for every subsequent case.
@@ -32,6 +37,12 @@ private:
 
     void pageFrame( cv::Mat );
 
+    std::vector<Target*>* getFullListPtr();
+
+    std::vector<Target*>* getPrevTargetPtr();
+
+    std::vector<Target*>* getNextTargetPtr();
+
     void setNextFrame( cv::Mat );
 
     cv::Mat getNextFrame();
@@ -40,13 +51,17 @@ private:
 
     cv::Mat getPrevFrame();
 
+    Detector* getDetectorPtr();
+
+    Selector* getSelectorPtr();
+
     std::vector<int> getTargetCoords( int );
 
     int getNumTargets();
 
     void clearPrevTargets();
-    
-    void Sentry::clearNextTargets()
+
+    void Sentry::clearNextTargets();
 
     void pageFrame( cv::Mat frame, int frame_num );
     
@@ -54,11 +69,17 @@ private:
 
     std::vector<Target*> getRelevantTargets();
 
+     void clearNextTargets();
+
     std::vector<float> getMeanTargetVelocity( std::vector<Target*> relevant_targets );
+
+    int findDebris( cv::Mat, int);
 
     void updateDebrisLikelihood( std::vector<Target*> relevant_targets );
 
     void Sentry::writeTargetsToFile(std::vector<Target*> full_target_list);
+
+    void updateDebrisLikelihood();
 
     void Sentry::dumpOldTargets();  
 };
