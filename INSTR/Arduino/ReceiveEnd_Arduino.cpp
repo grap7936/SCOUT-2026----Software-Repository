9208f0bc7cb11a2ab57fa6593495b08f83ef3341
grap7936 <grap7936@colorado.edu>
2026-07-13 13:58:31 -0600

    ReceiveEnd_Arduino.cpp

--- INSTR/Arduino/ReceiveEnd_Arduino.cpp
+++ INSTR/Arduino/ReceiveEnd_Arduino.cpp
@@ -7,7 +7,7 @@ Code Summary:
 
 Author: Graeme Appel
 
-Last Updated: 7/7/2026
+Last Updated: 7/13/2026
 */
 
 
@@ -55,7 +55,7 @@ void ArduinoReceiveClass::begin(unsigned long baudrate) {
 
 /////////////////////////////////////////////////////////////
 
-/* 2.) read()() function
+/* 2.) read() function
 
 Function description: This function checks for incoming data from the Jetson and unpacks it into a JetsonPackage struct. 
 It reads the data in a specific format, ensuring that the data is valid and complete before returning it.
@@ -72,14 +72,14 @@ Outputs:
 
 /////////////////////////////////////////////////////////////
 
-JetsonPackage ArduinoReceiveClass::read() {
+JetsonPackage ArduinoReceiveClass::read() { // structure containing object ID, x and y position and a boolean variable stating if there is a new data packet present
 
-    JetsonPackage Data_Package = {0, 0, 0, false};
+    JetsonPackage Data_Package = {0, 0, 0, false}; // before any data packets have been read in this must be initialized as 0's and false and then after data is read in this will be altered
 
-    if (Serial.available() >= 8) {
-        if (Serial.read() == '!') {
+    if (Serial.available() >= 8) { // ensures there are equal to or more than 8 bytes free meaning that there is at least 1 full data packet to be sent over
+        if (Serial.read() == '!') { // starting character check
 
-            // Read in each 8 bit segmeent of the 16 bit x and y coordinates and 16 bit target ID for each target.
+            // Read in each 8 bit segment of the 16 bit x and y coordinates and 16 bit target ID for each target. This is done in the same order described in the sending side in ArduinoSend.cpp.
             uint8_t id_high = Serial.read();
             uint8_t id_low = Serial.read();
             uint8_t x_high = Serial.read(); 
@@ -89,7 +89,8 @@ JetsonPackage ArduinoReceiveClass::read() {
             uint8_t end_marker = Serial.read();
 
             // If at the end of a given data packet sent over, assign and reassemble each 8 bit data packet into the original, full 16 bit data packages.
-            if (end_marker == '\n') {
+            if (end_marker == '\n') { // end marker check
+
                 Data_Package.id = (uint16_t)((id_high << 8) | id_low);
                 Data_Package.x  = (int16_t)((x_high << 8) | x_low);
                 Data_Package.y  = (int16_t)((y_high << 8) | y_low);
@@ -113,7 +114,8 @@ JetsonPackage ArduinoReceiveClass::read() {
 
 /* 3.) motor_test() function
 
-Function description: This function is used to test the motor functionality by setting a target angle and reporting the current position.
+Function description: This function is used to test the motor functionality by setting moving the motor forward 1 full revolution
+from its starting position and outputting the final angle back to the Jetson.
 
 Inputs: 
 None
@@ -184,5 +186,7 @@ void ArduinoReceiveClass::write(uint16_t FRAME_NUM, float CURRENT_MOTOR_POS) {
     Serial.print(FRAME_NUM);
     Serial.print(F("] Pos: "));
     Serial.println(CURRENT_MOTOR_POS, 4);
+
+
 };
 
