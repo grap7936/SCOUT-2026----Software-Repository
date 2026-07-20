2c45f0b1421b59ac42ef4b89dabdc15043c3bb07
grap7936 <grap7936@colorado.edu>
2026-07-20 11:33:31 -0600

    ArduinoMain.ino
    Version with 2 pin interrupt motor setup included as opposed to PWM setup if this is needed for later use.

--- INSTR/arduino/ArduinoMain.ino
+++ INSTR/arduino/ArduinoMain.ino
@@ -24,7 +24,7 @@ each function which helps to execute the required functionality, such as moving 
 
 Author: Graeme Appel
 
-Last Updated: 7/16/2026
+Last Updated: 7/20/2026
 */
 
 /////////////////////////////////////////////////////////////
@@ -37,50 +37,105 @@ Last Updated: 7/16/2026
 
 /////////////////////////////////////////////////////////////
 
-// Global variable definitions
+// --- Global variable definitions ---
 // volatile because these are written inside CamInterrupt() (an ISR) and read in loop().
 // Without volatile the compiler may cache a stale copy and never see the ISR's updates.
 volatile float CURRENT_MOTOR_POS = 0.0;
 volatile int FRAME_NUM = 0; // Frame number counter starting from when the camera begins sending and processing data each time after an interrupt -- initialized to 0 for the time being, this will be changed later in the code.
 extern float const Sentry_RPM = 10; // set the spinning motor speed to a default of 10 RPM. Likely this parameter won't be changed but if so the user input on the testingArduinoMain side can be altered
 
+// --- Hardware Pins ---
+const int ENCODER_PIN_A = 2; // encoder interrupt pins for the motor encoder are set to be 5 and 6 rather than 2 and 3 which are the default
+const int ENCODER_PIN_B = 3;
+// const int ENCODER_PIN = 3;   // PWM encoder signal (White wire) 
+
+// Updated from Pin 2 to Pin 4.
+// Pins 5 and 6 are completely dedicated to high-speed encoder phase matching
+// Pin 4 will now handle the camera's frame-sync pulse. -- this is done using the PinChangeInt library to reconfigure pin functionality
+// Note depending on Electrical team's preferences, this might have to be changed to another pin but is set as pin 4 for the time being.
+const int CAM_FRAME_PIN = 4;
+const uint8_t SLEEP_PIN = 7; // Sleep pin to control driver power state
+
 bool testModeActive = true;
 float motor_target_angle = 0.0; // Continuous absolute angle tracking variable for the testMotor function and for keeping track of angle for tracking -- in [rads]
 float motor_target_velocity = 0.0; // Continuous absolute velocity tracking variable for the Sentry mode and for keeping track of velocity for tracking -- in [rads/s]
 
-// Initialize necessary motor setup details: (subject to change later once we know the specific motor parameters)
+// --- Initialize necessary motor setup details: --- (subject to change later once we know the specific motor parameters)
 const int PPR = 2048; // Motor Resolution/PPR (Pulses Per Revolution) -- set as an arbitrary 2048 number for now before knowing accurate motor specs
 const int Pole_Pairs = 11; // number of Pole Pairs the given motor has -- set arbitrarily for now
-// Necessary camera parameters 
+
+// ---  Necessary camera parameters --- 
 float const CAM_FRAME_WIDTH_PX = 2464.0f; // camera frame width in pixels
 // float const CAM_FRAME_HEIGHT_PX = 2064.0f; // uncomment only if needed
 // float const CAM_FRAME_HEIGHT_RAD = 6.09  * (PI/180.0f);// camera frame height in radians -- FOV converted from degree input
 float const CAM_FRAME_WIDTH_RAD =  8.21 * (PI/180.0f); // camera frame width in radians -- FOV converted from degree input
 
+// --- SimpleFOC Setup ---
 // BLDC motor instance matching the number of pole pairs the motor has as an input 
 BLDCMotor motor = BLDCMotor(Pole_Pairs);
 
-// 2. Driver instance specifying the 3 PWM pins and the EN pin shown in your diagram
-BLDCDriver3PWM driver = BLDCDriver3PWM(11, 10, 9, 8);  // Inputs: 1.) IN Pin #1 connection on the Arduino to the simpleFOC board -- controls 1 pair of MOSFETs internally
+// Driver instance specifying the 3 PWM pins and the EN pin shown in your diagram
+BLDCDriver3PWM driver = BLDCDriver3PWM(9, 10, 11, 8);  // Inputs: 1.) IN Pin #1 connection on the Arduino to the simpleFOC board -- controls 1 pair of MOSFETs internally
                                                        //         2.) IN Pin #2 connection on the Arduino to the simpleFOC board -- controls 1 pair of MOSFETs internally
                                                        //         3.) IN Pin #3 connection on the Arduino to the simpleFOC board -- controls 1 pair of MOSFETs internally
                                                        //         4.) Enable Pin number that the simpleFOC board is attached to the Arduino -- acts as the master hardware safety switch for the motor driver board.
 
-// Encoder instance utilizing the interrupt pins 2 and 3
-Encoder encoder = Encoder(2, 3, PPR); // Inputs: 1.) Digitial PWM/Interrupt pin plug in #1
+// Encoder instance utilizing the interrupt pins 5 and 6
+Encoder encoder = Encoder(ENCODER_PIN_A, ENCODER_PIN_B, PPR); // Inputs: 1.) Digitial PWM/Interrupt pin plug in #1
                                       //         2.) Digitial PWM/Interrupt pin plug in #2
                                       //         3.) Motor PPR (Pulses Per Revolution) -- i.e basically the motors resolution
+
+// MagneticSensorPWM sensor = MagneticSensorPWM(ENCODER_PIN, 4, 904); // Inputs: 1.) _pinPWM == Arduino pin connected to the PWM (pulse width modulation) output of the magnetic sensor
+//                                                                    //         2.) _min_raw_count == minimum expected length of the PWM pulse in [microseconds]
+//                                                                    //         3.) _max_raw_count == maximum expected length of the PWM pulse in [microseconds]
+
+// void doPWM(){ sensor.handlePWM(); }
+
 
 // Interrupt service routines required by SimpleFOC
 void doA() { encoder.handleA(); }
 void doB() { encoder.handleB(); }
 
-// Updated from Pin 2 to Pin 4.
-// Pins 2 and 3 are completely dedicated to high-speed encoder phase matching
-// Pin 4 will now handle the camera's frame-sync pulse. -- this is done using the PinChangeInt library to reconfigure pin functionality
-// Note depending on Electrical team's preferences, this might have to be changed to another pin but is set as pin 4 for the time being.
 
-const int CAM_FRAME_PIN = 4;
+/////////////////////////////////////////////////////////////
+
+// User Defined Functions and Structures
+
+
+// --- Custom PID Data Structure ---
+struct PID {
+  float Kp, Ki, Kd, Kaw, T_C, T, max, min, max_rate;
+  float integral, err_prev, deriv_prev, command_sat_prev, command_prev;
+};
+
+
+struct PID motor_pid; //  Instantiated the global PID structure object
+
+
+// Custom PID execution function
+float PID_Step(struct PID *pid, float measurement, float setpoint) {
+  float err = setpoint - measurement;
+  pid->integral += pid->Ki * err * pid->T + pid->Kaw * (pid->command_sat_prev - pid->command_prev) * pid->T;
+  float deriv_filt = (err - pid->err_prev + pid->T_C * pid->deriv_prev) / (pid->T + pid->T_C);
+
+  pid->err_prev = err;
+  pid->deriv_prev = deriv_filt;
+
+  float command = pid->Kp * err + pid->integral + pid->Kd * deriv_filt;
+  pid->command_prev = command;
+
+  float command_sat = command;
+  if (command > pid->max) command_sat = pid->max;
+  else if (command < pid->min) command_sat = pid->min;
+
+  if (command_sat > pid->command_sat_prev + pid->max_rate * pid->T)
+    command_sat = pid->command_sat_prev + pid->max_rate * pid->T;
+  else if (command_sat < pid->command_sat_prev - pid->max_rate * pid->T)
+    command_sat = pid->command_sat_prev - pid->max_rate * pid->T;
+
+  pid->command_sat_prev = command_sat;
+  return command_sat;
+}
 
 
 // Camera / External Hardware Frame Interrupt
@@ -106,14 +161,16 @@ void setup() {
   // Initializing the hardware layers using built-in library functions
   encoder.init(); // sets up the embedded software to read electrical square waves and convert to dial inputs
 
+  encoder.enableInterrupts(doA, doB); // enables changing interrupt pins from the default system pins of 2 and 3 to other pins. The inputs simply link the pin changes to the embedded interrupt functions doA and doB performed in the code.
+
   // Hardware-links the external physical changes on Digital Pins 2 and 3 directly to your user-defined background functions (doA and doB).
-  attachInterrupt(digitalPinToInterrupt(2), doA, CHANGE);  // Hardware-links the external physical changes on Digital Pins 2 and 3 directly
+  attachInterrupt(digitalInterrupt(ENCODER_PIN_A), doA, CHANGE);  // Hardware-links the external physical changes on Digital Pins 2 and 3 directly
                                                            // to your user-defined background functions (doA and doB). Standard code loops
                                                            // execute sequentially, which is too slow to catch high-speed motor signals. If
                                                            //  the motor spins and your Arduino is busy waiting for a serial data packet from the
                                                            //  Jetson, it will miss the encoder steps entirely. An interrupt instantly pauses the main 
                                                            // program loop for a fraction of a microsecond to record the exact movement step, ensuring 100% accurate position tracking.
-  attachInterrupt(digitalPinToInterrupt(3), doB, CHANGE);
+  attachInterrupt(digitalInterrupt(ENCODER_PIN_B), doB, CHANGE);
 
   // Attach the camera frame-sync interrupt so FRAME_NUM increments and CURRENT_MOTOR_POS
   // latches on each frame event. Without this, write() would always report Frame [0] Pos: 0.0000.
@@ -125,11 +182,24 @@ void setup() {
   pinMode(CAM_FRAME_PIN, INPUT);
   attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(CAM_FRAME_PIN), CamInterrupt, RISING);
 
+  // Configure Sleep Pin to keep the driver awake before driver.init()
+  pinMode(SLEEP_PIN, OUTPUT);
+  digitalWrite(SLEEP_PIN, HIGH); // HIGH disables sleep mode (turns driver ON)
+  driver.voltage_power_supply = 12.0;
+
   driver.init(); // Configures pins 11, 10, and 9 as high-frequency PWM outputs and sets the hardware enable pin (Pin 8) to wake up the SimpleFOC Mini gate driver chip.
     
   // Linking the pieces together using built-in library functions
   motor.linkDriver(&driver); // Directly pairs your motor math object with the physical driver instance you configured on pins 11, 10, 9, and 8.
   motor.linkSensor(&encoder); // Hardware-links the external physical changes on Digital Pins 2 and 3 directly to your user-defined background functions (doA and doB).
+
+  
+  // Initialize Custom PID Parameters and Configure SimpleFOC for Voltage/Torque limits
+  motor_pid = { 4.5, 1.2, 0.05, 0.3, 0.01, 0.001, 3.0, -3.0, 100.0, 0, 0, 0, 0, 0 };
+  motor.voltage_limit = 3.0; 
+  motor.controller = MotionControlType::torque; 
+  motor.torque_controller = TorqueControlType::voltage;
+
 
   // Booting the FOC math engine
   motor.init(); // Allocates system memory for the tracking variables and loads your motor parameters (like the 11 pole pairs) into the controller.
@@ -161,16 +231,29 @@ void loop() {
         if (!testModeActive && Data_Package.y >= 0) {
             
             // >= 0 -> target_position = PID(Command)
-            motor.controller = MotionControlType::angle;
+            motor.controller = MotionControlType::torque; // torque/voltage control for the motor allows the system to be able to bypass the SimpleFOC libary's position and
+                                                          // velocity algorithms and simply apply specific voltages to the motor coils which integrates much more efficiently 
+                                                          // with the provided PID code in the system. Additionally, this allows the PID loop and motor to react efficiently 
+                                                          // and instantly to sudden changes in objects in the frame and this allows the motor to help the camera to track the data
+                                                          //  with a much larger degree of accuracy.
+
+            // Real-Time dt (Sample Time) Calculation for PID
+            static unsigned long last_time = micros();
+            unsigned long now = micros();
+            float dt = (now - last_time) / 1000000.0;
+            last_time = now;
+            
+            if (dt <= 0.0 || dt > 0.1) dt = 0.001; 
+            motor_pid.T = dt; 
 
             // Compute the angular error between the target position and the center of the frame
             float rad_diff = ArduinoReceive.getRadDiff(CAM_FRAME_WIDTH_PX, CAM_FRAME_WIDTH_RAD, Data_Package.x);
             
-            // -------------------------------------------------------------
-            // Pass rad_diff to Mansi's PID code which then the result of that will be passed to the motor to move towards the target
-            // ex: the line after the PID code would just be: motor_target_angle = motor.shaft_angle + rad_diff_after_PID; or just simply: motor_target_angle = MansiPID(rad_diff);
-            // -------------------------------------------------------------
-            
+            //  Pass calculated parameters through the custom PID sequence to establish control voltage
+            float setpoint = motor.shaft_angle + rad_diff;
+            float control_voltage = PID_Step(&motor_pid, motor.shaft_angle, setpoint);
+            motor.move(control_voltage);
+
         } 
 
         // reject live (y >= 0) packets while still in test mode 
@@ -182,7 +265,7 @@ void loop() {
             // SAFETY FILTER: If we are in tracking mode, ignore any testing commands (y < 0) 
             // EXCEPT for state changes like -5 (enable tracking) or -6 (return to test) as well as -1 (starting sentry mode)
             if (!testModeActive && Data_Package.y != -5 && Data_Package.y != -6 && Data_Package.y != -1) {
-                Serial.println(F("[BLOCKED] Testing command rejected. System is in TRACKING_MODE."));
+                Serial.println(F("[BLOCKED]")); // Blocks data packets if not in tracking mode.
             }
 
             else { 
@@ -206,7 +289,11 @@ void loop() {
 
                     case -5: // Disable testing bounds pipeline -> engage live target tracking
                         testModeActive = false;
-                        motor.controller = MotionControlType::angle;
+                        motor.controller = MotionControlType::angle; // (Resetting to angle momentarily until tracking loop sets to torque)
+                                                                     // This is necessary because if the motor were to start in tracking mode it would have a default voltage input of 0 volts which
+                                                                     // could cause the motor to stop moving or physically drop to being limp which could effect the inertia of the payload. By being shortly 
+                                                                     // in angle mode, this allows the system to actively hold the motor position while transitioning to tracking mode so that when torque mode
+                                                                     // enagages, voltage inputs can be used effectively.
                         motor_target_angle = 0.0;
                         Serial.println(F("SYSTEM STATE UPDATE: Switching to TRACKING_MODE."));
                         break;
@@ -227,10 +314,13 @@ void loop() {
     }
 
     // Pass target parameters to physical driver constraints
-    if (motor.controller == MotionControlType::velocity) {
-        motor.move(motor_target_velocity);
-    } else {
-        motor.move(motor_target_angle);
+    // Wrapped in if(testModeActive) so the general motor.move() doesn't overwrite the tracking voltage command
+    if (testModeActive) {
+        if (motor.controller == MotionControlType::velocity) {
+            motor.move(motor_target_velocity);
+        } else {
+            motor.move(motor_target_angle);
+        }
     }
 
     // Execute synchronous pipeline updates back to the Linux application host
